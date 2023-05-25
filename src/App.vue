9aeaaeafa1861f8b6d8ac01418049ff29c6ce0c6
Masahiko Shin <contact@shin-translation.com>
2023-05-25 15:58:34 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <div
+      v-for="i in [...Array(2000).keys()]" :key="i" 
+      class="rect"
+    >
+      This element disappears when you scroll quickly up and down on an iOS browser.
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.rect {
+  background-color: #0dd;
+}
+</style>
